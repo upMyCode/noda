802aa4e9a5f28aa8e0c78ba8dbf51d62a4cbeb19
Ulad Birukou <alorine99@gmail.com>
2021-03-19 20:52:43 +0300

    server-node-part2

--- firstServerNode.js
+++ firstServerNode.js
@@ -1,12 +1,25 @@
-const http = require('http');
-const hostName = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end(`Hello World\n`)
+// const http = require('http');
+// const hostName = '127.0.0.1';
+// const port = 3000;
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200
+//     res.setHeader('Content-Type', 'text/plain')
+//     res.end(`Hello World\n`)
+// })
+
+// server.listen(port, hostName, () => {
+//     console.log(`Server running at http://${hostName}:${port}/`)
+// })
+
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => {
+    res.send('Hi!')
 })
 
-server.listen(port, hostName, () => {
-    console.log(`Server running at http://${hostName}:${port}/`)
+app.listen(4000, () => console.log('Server done'))
+process.on('SIGTERM', () => {
+    app.close(() => {
+        console.log('Thats end')
+    })
 })
